5bc5b3cbf1e3e7964ad1b6edb0223fbc31702aa7
Khadga Shrestha <khadgalovecoding2016@gmail.com>
2024-07-25 08:02:10 +0545

    added ingress service

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -4,6 +4,21 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: [],
+    message: "Auth Service is working fine.",
+  });
+});
+
+app.get("/demo", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: [],
+    message: "Demo is working fine.",
+  });
+});
 app.listen(3000, () => {
   console.log("(auth) Listening on port 3000");
 });
